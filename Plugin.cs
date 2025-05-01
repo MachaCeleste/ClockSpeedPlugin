7afa05a586dbf57a7903a48c0a8e3a7a154a809e
Macha Celeste <shetah1@yahoo.com>
2025-05-01 11:50:54 -0400

    Add project files.

---
+++ Plugin.cs
@@ -0,0 +1,24 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using HarmonyLib;
+
+namespace ClockSpeedPlugin;
+
+[BepInPlugin("com.machaceleste.clockspeedplugin", MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+public class Plugin : BaseUnityPlugin
+{
+    internal static new ManualLogSource Logger;
+
+    public static ConfigEntry<int> timeDiv;
+
+    private void Awake()
+    {
+        timeDiv = Config.Bind("Main", "Time Divisor", 14, new ConfigDescription("Sets the speed the clock runs at, must be set before singleplayer launches, default: 14:1", new AcceptableValueRange<int>(1, 32)));
+
+        Logger = base.Logger;
+        Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+        var harmony = new Harmony("com.machaceleste.clockspeed");
+        harmony.PatchAll();
+    }
+}
---
+++ patches/ClockServerPatch.cs
@@ -0,0 +1,15 @@
+﻿using ClockSpeedPlugin;
+using HarmonyLib;
+
+[HarmonyPatch]
+public class ClockServerPatch
+{
+    [HarmonyPatch(typeof(ClockServer), "ConfigureServer")]
+    class ConfigureServerPatch
+    {
+        static void Prefix()
+        {
+            Clock.multTiempo = Plugin.timeDiv.Value;
+        }
+    }
+}
